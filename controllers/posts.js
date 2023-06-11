9dc7e4945c3f8da2636629ec785b13ae81c6ffe4
Ihor Statsenko <statsenkoin@gmail.com>
2023-06-11 16:51:10 +0300

    Create services for app and cloudinary storages

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,24 +1,9 @@
-import fs from 'fs/promises';
-import path from 'path';
-
 import Post from '../models/post.js';
 import ctrlWrapper from '../helpers/ctrlWrapper.js';
 import HttpError from '../helpers/HttpError.js';
 
-// =====================================================
-// const cloudinary = require('cloudinary').v2;
-import cloudinary from 'cloudinary';
-// CLOUDINARY_API_KEY=894973229482847
-// CLOUDINARY_SECRET_KEY=CIXo3ayG1wPxbI_5_8M0BTGuTUo
-// CLOUDINARY_CLOUD_NAME=dmrnrkgjn
-cloudinary.config({
-  cloud_name: 'dmrnrkgjn',
-  api_key: '894973229482847',
-  api_secret: 'CIXo3ayG1wPxbI_5_8M0BTGuTUo',
-});
-// =====================================================
-
-const imageDir = path.join(process.cwd(), 'public', 'images');
+import appStorage from '../services/appStorage.js';
+import cloudinaryStorage from '../services/cloudinaryStorage.js';
 
 const getAll = async (req, res, next) => {
   const posts = await Post.find();
@@ -27,38 +12,28 @@ const getAll = async (req, res, next) => {
 
 const createPost = async (req, res, next) => {
   const { path: tempUpload, filename } = req.file;
-  console.log('req.file :>> ', req.file);
-
-  // Cloudinary storage ========================================
-  const fileData = await cloudinary.v2.uploader.upload(
-    tempUpload,
-    { folder: 'posts' },
-    function (error, result) {
-      console.log(result);
-    }
-  );
-  await fs.unlink(tempUpload);
-  const imagePath = fileData.url;
-  // =====================================================
-
-  // app storage =========================================
-  // const { path: tempUpload, filename } = req.file;
-  // const resultUpload = path.join(imageDir, filename);
-  // await fs.rename(tempUpload, resultUpload);
-  // const imagePath = path.join('public', 'images', filename);
-
-  // =====================================================
+  // storage ==========================================
+  // const imagePath = await appStorage.saveImage(tempUpload, filename);
+  const imagePath = await cloudinaryStorage.saveImage(tempUpload);
+  // ==================================================
   const newPost = { ...req.body, imagePath };
   const post = await Post.create(newPost);
+
   res.status(201).json(post);
 };
 
 const deletePost = async (req, res, next) => {
   const { id } = req.params;
+
   const post = await Post.findByIdAndDelete(id);
   if (!post) {
     throw HttpError(404, 'Post not found');
   }
+  // storage ==========================================
+  // await appStorage.removeImage(post);
+  await cloudinaryStorage.removeImage(post);
+  // ==================================================
+
   res.json({ message: 'Post deleted' });
 };
 
